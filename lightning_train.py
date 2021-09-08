cd5e8bc2aaba0a0b3544153f84fad26fdadf979a
itruonghai <nguyentruonghai141@gmail.com>
2021-09-08 10:53:21 +0700

    early stopping

--- lightning_train.py
+++ lightning_train.py
@@ -21,21 +21,29 @@ checkpoint_callback = ModelCheckpoint(
     save_last= True,
     auto_insert_metric_name=False
 )
+early_stop_callback = EarlyStopping(
+   monitor='val/MeanDiceScore',
+   min_delta=0.0001,
+   patience=15,
+   verbose=False,
+   mode='max'
+)
 tensorboardlogger = TensorBoardLogger(
     'logs', 
     name = args.exp, 
     default_hp_metric = None 
 )
-trainer = pl.Trainer(   #fast_dev_run = 10, 
-                        # accelerator='ddp',
-                        # overfit_batches=5,
-                        gpus = [0], 
+trainer = pl.Trainer(#fast_dev_run = 10, 
+#                     accelerator='ddp',
+                    #overfit_batches=5,
+                     gpus = [0], 
                         precision=16,
-                        max_epochs = 200, 
-                        progress_bar_refresh_rate=10,  
-                        callbacks=[checkpoint_callback], 
-                        num_sanity_val_steps=2,
-                        logger = tensorboardlogger,
+                     max_epochs = 200, 
+                     progress_bar_refresh_rate=10,  
+                     callbacks=[checkpoint_callback, early_stop_callback], 
+#                     auto_lr_find=True,
+                    num_sanity_val_steps=2,
+                    logger = tensorboardlogger,
 #                     limit_train_batches=0.01, 
 #                     limit_val_batches=0.01
                      )
